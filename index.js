0dece515544c6710589748dcbb0d57ef2c014ac6
Kevin Grimaldi <anthgrim@icloud.com>
2022-10-14 16:13:34 -0400

    testing

---
+++ index.js
@@ -0,0 +1,33 @@
+/**
+ * @desc  Takes a number and returns it as currency format
+ * @param {number} amount
+ * @param {string} countryCode
+ * @param {string} style
+ * @param {string} currencyCode
+ */
+const convertToMoneyString = (amount, countryCode, style, currencyCode) => {
+  return new Intl.NumberFormat(countryCode, {
+    style: style,
+    currency: currencyCode,
+  }).format(amount);
+};
+
+/**
+ * @desc  Takes a date-like string and returns a formatted date with the specified country code
+ * @param {string} date
+ * @param {string} countryCode
+ * @returns string
+ */
+const convertToDateString = (date, countryCode) => {
+  const newDate = new Date(date);
+  newDate.setMinutes(newDate.getMinutes() + newDate.getTimezoneOffset());
+
+  return new Intl.DateTimeFormat(countryCode).format(newDate);
+};
+
+const generalFormatter = {
+  convertToMoneyString,
+  convertToDateString,
+};
+
+module.exports = generalFormatter;
